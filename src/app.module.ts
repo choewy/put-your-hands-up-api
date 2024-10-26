5eb992432f476efc00d2452a308e5dcead1388b1
choewy <choewy32@gmail.com>
2024-10-27 00:11:05 +0900

    feature: health module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ApplicationModule } from './application';
 import { ConfigFactoryModule } from './common';
 import { ContextModule, LoggerModule } from './core';
 
 @Module({
-  imports: [ConfigFactoryModule.forRoot(), ContextModule.forRoot(), LoggerModule.forRoot()],
+  imports: [ConfigFactoryModule.forRoot(), ContextModule.forRoot(), LoggerModule.forRoot(), ApplicationModule],
   controllers: [AppController],
   providers: [AppService],
 })
